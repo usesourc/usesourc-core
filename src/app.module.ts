79489ac0d128eddda7aeaa70d877fc09a42a2554
Ali Furkan Demirbaş <furkan.demirbas@usemagnetiq.com>
2022-01-10 23:58:12 +0300

    app module dependency injections

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { RepositoriesModule } from './repositories/repositories.module';
+import { LogsModule } from './logs/logs.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, NotificationsModule, RepositoriesModule, LogsModule],
   controllers: [AppController],
   providers: [AppService],
 })
